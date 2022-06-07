69a9f4a4f3b5e76bfa685e4fda35142be1edf8ed
Zabinyyo <zabinyyo@gmail.com>
2022-06-07 13:04:45 +0300

    float number

---
+++ float_number.py
@@ -0,0 +1,7 @@
+#input the float number
+your_type = input("Enter a float ")
+#Convert input to a float number
+your_type = float(your_type)
+#Convert input to a float number
+print("Round to 2 decimal place: {:.2} ".format(your_type))
+
