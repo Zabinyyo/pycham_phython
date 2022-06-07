887a07f7bb36a47f0fbcb3b23ad45ee53cfa581d
Zabinyyo <zabinyyo@gmail.com>
2022-06-07 12:55:12 +0300

    for_loop_range and odd values

---
+++ for_loop_range.py
@@ -0,0 +1,16 @@
+#Output for range is 0-19
+for i in range(20):
+    # print out the numbers range
+    print("i = ", i)
+
+#Output for range is 2-19
+for i in range(2,20):
+    #print out the numbers range
+    print("i '=' ", i)
+
+#Output all odd numbers from 1-21
+for i in range(1,21):
+    #Use module to check that result is not equal to 0
+    if((i % 2) != 0):
+    #print odd
+        print("i = ", i)
