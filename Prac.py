80350f318fd0997363cb2bd376805989343aae79
Zabinyyo <zabinyyo@gmail.com>
2022-06-09 00:12:30 +0300

    Prac file

---
+++ Prac.py
@@ -0,0 +1,24 @@
+for i in ["Hello", "Holberton", "School", 98]:
+    print(i, end=" ")
+
+#com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
+#git clone https://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@github.com/Zabinyyo/alx-pre_course.git
+
+#https://www.datacamp.com/community/tutorials/git-push-pull.copr_http.cos://www.git-tower.com/learn/git/faq/git-rename-master-to-main
+#git/
+
+#alx-system_engineering-devops
+
+#Dkazem91/holberton-system_engineering-devops
+
+#https://github.com/Pericles001/alx-higher_level_programming/tree/main/0x01-python-if_else_loops_functions
+#https://github.com/Emmastro/alx-higher_level_programming/tree/master/0x04-python-more_data_structures
+#https://github.com/Swirlboy/alx-system_engineering-devops/blob/main/0x02-shell_redirections/6-third_lineE
+#https://github.com/davidmusau/alx-low_level_programming/tree/main/0x00-hello_world
+#https://www.programiz.com/python-programming/online-compiler/
+#https://www.youtube.com/watch?v=3loZ0NOc4Wc&list=PLWPirh4EWFpEK1RbpruFXrwgzcGUjZ4nV&index=4
+#https://github.com/NeuralNine
+#https://github.com/mcantor/no_plugins
+180928
+#https://careers.un.org/lbw/jobdetail.aspx?id=180928
+#https://github.com/Heyeman/alx-higher_level_programming/blob/main/0x00-python-hello_world/0-run
